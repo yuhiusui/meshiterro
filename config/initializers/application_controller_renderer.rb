f5c9ab54396ea9b3d1a408529cc17173e872df52
yuhiusui <usui.hanshin@gmail.com>
2020-03-24 19:06:29 +0900

    [finish]5-3

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '37fbecc0de1e5bf664653efe8a3a8ced475c74b96db3c1a805d069147b4a722fb089aebc43fe806077c8ccdb515c05de2ffcae93c76eae63ad3c247fa1f2ce83'
+Refile.secret_key = '6df5bc9057c42be6d35641058f067ee86a177e5f0563d488cc319b4b49f779f8a788e2969bf95f38cd5cd1016ed271b91e848401354ff8377a33d8cf798682fc'
